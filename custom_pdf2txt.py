aa11cd932a15435470a8fef69f5251881eb77573
Hong <khfy6hzy@nottingham.edu.my>
2018-11-09 18:14:03 +0800

    pdf2txt script and test texts

---
+++ custom_pdf2txt.py
@@ -0,0 +1,39 @@
+import io
+
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+
+
+def convert_pdf_to_txt(path,newfilename):
+    rsrcmgr = PDFResourceManager()
+    retstr = io.StringIO()
+    codec = 'utf-8'
+    laparams = LAParams()
+    device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+    fp = open(path, 'rb')
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    password = ""
+    maxpages = 0
+    caching = True
+    pagenos = set()
+
+    for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages,
+                                  password=password,
+                                  caching=caching,
+                                  check_extractable=True):
+        interpreter.process_page(page)
+
+    text = retstr.getvalue()
+
+    fp.close()
+    device.close()
+    retstr.close()
+
+    #custom convert to file.
+    #newfilename format should be 'xxx.txt'
+    converted_file = open(newfilename,'w+',encoding=codec)
+    converted_file.write(text)
+    converted_file.close()
+    return text
