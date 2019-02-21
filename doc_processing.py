07125593a2e449bdd9e2272d2f9d9514f6f87ec1
Hong <khfy6hzy@nottingham.edu.my>
2019-02-22 02:14:58 +0800

    Send HTTP request to localhost in parallel to speed up tagging

--- doc_processing.py
+++ doc_processing.py
@@ -1,9 +1,9 @@
-import nltk , custom_pdf2txt , sparql , pathlib , time, string
+import nltk , custom_pdf2txt , sparql , pathlib , time, string , asyncio, concurrent.futures
 
 from custom_pdf2txt import convert_pdf_to_txt
 from nltk import word_tokenize
 from nltk.corpus import stopwords
-from sparql import getConceptTagVirtuoso , getIntersectionOfNTriplesVirtuoso
+from sparql import getConceptTagVirtuoso , getNTriplesFromConceptVirtuoso , getIntersectionOfNTriplesVirtuoso
 
 #helper function to stem out numbers
 def is_number(s):
@@ -12,6 +12,50 @@ def is_number(s):
         return True
     except ValueError:
         return False
+
+async def taggingHelper(arr):
+    for a in arr:
+        a = a.replace('\\','')
+
+    loop = asyncio.get_event_loop()
+    futures = [
+        loop.run_in_executor(
+            None,
+            getConceptTagVirtuoso,
+            word
+            )
+        for word in arr
+    ]
+    responses =  await asyncio.gather(*futures)
+    return responses
+
+async def ntripleHelper(arr):
+    loop = asyncio.get_event_loop()
+    futures = [
+        loop.run_in_executor(
+            None,
+            getNTriplesFromConceptVirtuoso,
+            word['baseTag']
+            )
+        for word in arr
+    ]
+    responses = await asyncio.gather(*futures)
+    return responses
+
+def ntripleIntersectionHelper(arr):
+    ntriples = []
+    loop = asyncio.get_event_loop()
+    res = loop.run_until_complete(ntripleHelper(arr))
+
+    for index , r in enumerate(res):
+        potentialNTriples = r['results']['bindings']
+        for keyword2 in arr:
+            for potentialNTriple in potentialNTriples:
+                if potentialNTriple['o']['value'].find(keyword2['baseTag']) >= 0:
+                    tempStr = arr[index]['baseTag'] + " {} ".format(potentialNTriple['p']['value']) + keyword2['baseTag']
+                    ntriples.append(tempStr)
+
+    return ntriples
 
 def process_pdf2txt(path):
     newfilename = pathlib.Path(path).stem + '.txt'
@@ -77,6 +121,13 @@ def process_pdf2txt(path):
 
 #returns tagged keywords that have correspond entry in AGROVOC
 def process_txt(path):
+    
+    path = pathlib.Path(path)
+
+    if path.stem.find('_keywords') != -1 or path.stem.find('_ntriples') != -1:
+        return
+
+    print('Now processing {}\n'.format(path.stem)) 
     file = open(path,'r',encoding="utf-8")
     text = file.read()
 
@@ -90,11 +141,12 @@ def process_txt(path):
     vocab = sorted(set(text))
     
     #remove all the number tokens
+    cutoff = 0
     for v in vocab:
         if v.startswith('a'):
             cutoff = vocab.index(v)
             break
-    vocab = vocab[cutoff:len(vocab)-1]
+    vocab = vocab[cutoff:len(vocab)]
     
     #remove all numbers and punctuation and stopwords
     customStopwords = stopwords.words('english') + list(string.punctuation)
@@ -104,10 +156,15 @@ def process_txt(path):
 
     #may return empty after query AGROVOC , thus is potential keywords
     potential_keywords = []
+    loop = asyncio.get_event_loop()
+    tempConcept = loop.run_until_complete(taggingHelper(vocab))
+
     for v in vocab:
         # the foward slash is a syntax error in SPARQL 
         v = v.replace('\\','')
-        potential_keywords.append({'word':v , 'baseTag': getConceptTagVirtuoso(v)})
+
+    for v , concept in zip(vocab,tempConcept):
+        potential_keywords.append({'word':v , 'baseTag':concept})
 
     #cleaning up the query results by removing empty responses
     #format of query output refer to GitHub
@@ -132,7 +189,7 @@ def process_txt(path):
     ntriplesFile = pathlib.Path(path).parent / ntriplesFile
 
     _file = open(ntriplesFile , 'w+' , encoding='utf-8')
-    ntriples = getIntersectionOfNTriplesVirtuoso(tagged_keywords)
+    ntriples = ntripleIntersectionHelper(tagged_keywords)
     for nt in ntriples:
         _file.write(nt + '\n')
 
