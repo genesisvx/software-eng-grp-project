1c45e51f0fdbb0b4f20de08d8a8021f2dea265ad
Hong <khfy6hzy@nottingham.edu.my>
2019-03-11 01:08:44 +0800

    Add in bigrams & trigrams functionality and fix issue where pdf to text conversion hangs

--- custom_pdf2txt.py
+++ custom_pdf2txt.py
@@ -6,34 +6,39 @@ from pdfminer.layout import LAParams
 from pdfminer.pdfpage import PDFPage
 
 
-def convert_pdf_to_txt(path,newfilename):
-    rsrcmgr = PDFResourceManager()
-    retstr = io.StringIO()
-    codec = 'utf-8'
-    laparams = LAParams()
-    device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
-    fp = open(path, 'rb')
-    interpreter = PDFPageInterpreter(rsrcmgr, device)
-    password = ""
-    maxpages = 0
-    caching = True
-    pagenos = set()
+def convert_pdf_to_txt(path,newfilename , queue):
+    try:
+        rsrcmgr = PDFResourceManager()
+        retstr = io.StringIO()
+        codec = 'utf-8'
+        laparams = LAParams()
+        device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+        fp = open(path, 'rb')
+        interpreter = PDFPageInterpreter(rsrcmgr, device)
+        password = ""
+        maxpages = 0
+        caching = True
+        pagenos = set()
 
-    for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages,
-                                  password=password,
-                                  caching=caching,
-                                  check_extractable=True):
-        interpreter.process_page(page)
+        for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages,
+                                      password=password,
+                                      caching=caching,
+                                      check_extractable=True):
+            interpreter.process_page(page)
 
-    text = retstr.getvalue()
+        text = retstr.getvalue()
 
-    fp.close()
-    device.close()
-    retstr.close()
+        fp.close()
+        device.close()
+        retstr.close()
 
-    #custom convert to file.
-    #newfilename format should be 'xxx.txt'
-    converted_file = open(newfilename,'w+',encoding=codec)
-    converted_file.write(text)
-    converted_file.close()
-    return text
+        #custom convert to file.
+        #newfilename format should be 'xxx.txt'
+        converted_file = open(newfilename,'w+',encoding=codec)
+        converted_file.write(text)
+        converted_file.close()
+        queue.put(text)
+    except Exception:
+        print("can't convert\n")
+        text = ''
+        queue.put(text)
